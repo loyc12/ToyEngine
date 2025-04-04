d7333e5b9c4520c5b7763f58655321274fff6444
loyc12 <lordloyc@gmail.com>
2025-04-03 21:28:02 -0400

    shape2 compiles but is untested

--- incs/base/types/Shape2.hpp
+++ incs/base/types/Shape2.hpp
@@ -1,11 +1,16 @@
 #ifndef SHAPE2_HPP
 # define SHAPE2_HPP
 
+# include <raylib.h>
 # include "../defs.hpp"
 
 typedef vector< Vector2 > V2Vect_t; // TODO : move me
 
-V2Vect_t calcPolyVerts( byte_t facets );
+V2Vect_t CalcPolyVerts(   byte_t facets );
+Vector2  RotateVertex(    const Vector2 &v, float angle );
+float    getTriangleArea( const Vector2 &p1, const Vector2 &p2, const Vector2 &p3 );
+float    getDistance(     const Vector2 &p1, const Vector2 &p2 );
+float    getDistanceTo00( const Vector2 &p );
 
 typedef enum : byte_t
 {
@@ -30,14 +35,14 @@ class Shape2
 	protected:
 	// ================================== MEMBERS
 		sh2_type_e _type;
+		float      _angle;
 		Vector2    _center;
 		Vector2    _scales;
-		float      _angle;
 		V2Vect_t   _verts;
 
 	public:
 	// ================================== CONSTRUCTORS / DESTRUCTORS
-		inline Shape2() : _type( SH2_NULL ), _center( Vector2() ), _scales({ 1, 1 }), _angle( 0 ) { _verts = V2Vect_t(); };
+		inline Shape2() : _type( SH2_NULL ), _angle( 0 ), _center( Vector2() ), _scales({ 1, 1 }) { _verts = V2Vect_t(); };
 
 		Shape2( sh2_type_e type, const Vector2 &ctr, const Vector2 &scl = { 1, 1 }, float angle = 0 );
 
@@ -60,7 +65,7 @@ class Shape2
 		// returns a line going from p through the center to its opposite
 		static Shape2 Line3( const Vector2 &ctr, const Vector2 &p );
 
-		static Shape2 Triangle( const Vector2 &p1,  const Vector2 &tip );
+		static Shape2 Triangle( const Vector2 &ctr, const Vector2 &tip );
 		static Shape2 Triangle( const Vector2 &ctr, const Vector2 &scl, float angle );
 		static Shape2 Triangle( const Vector2 &p1,  const Vector2 &p2,  const Vector2 &p3 );
 
@@ -90,17 +95,22 @@ class Shape2
 		static Shape2 Polygon( const Vector2 &ctr, const Vector2 &scl, int sides, float angle = 0 );
 
 	// ================================== ACCESSORS
-		inline sh2_type_e     getType()       const { return _type; };
-		inline byte_t 			  getFacetCount() const { return _verts.size(); };
-		inline const Vector2  &getCenter()    const { return _center; };
-		inline const Vector2  &getScales()    const { return _scales; };
-		inline const V2Vect_t &getRawVerts()  const { return _verts; };
+		inline sh2_type_e     getType()    const { return _type; };
+		inline byte_t 			  getFacetC()  const { return _verts.size(); };
+		inline const Vector2  &getCenter() const { return _center; };
+		inline const Vector2  &getScales() const { return _scales; };
 
 		inline float   getAngle()       const { return _angle; };
 		inline Vector2 getAngleVector() const { return { cosf( _angle ), sinf( _angle )}; };
 
-		Vector2   getScaledVertex( int index ) const;
-		V2Vect_t  getScaledVerts() const;
+		inline const V2Vect_t &getRawVerts()  const;
+		V2Vect_t getScaledVerts() const;
+		V2Vect_t getWorldVerts() const;
+
+		// NOTE : Wraps around the index
+		Vector2  getRawVertex(    int index ) const;
+		Vector2  getScaledVertex( int index ) const;
+		Vector2  getWorldVertex(  int index ) const;
 
 	// ================================== SHAPE PROPERTIES
 		Vector2 getTop()   const;
@@ -113,14 +123,33 @@ class Shape2
 		Vector2 getBotLeft()  const;
 		Vector2 getBotRight() const;
 
-		float getWidth()  const;
-		float getHeight() const;
-		float getArea()   const;
-		float getPerim()  const;
+		float getMinY() const;
+		float getMaxY() const;
+		float getMinX() const;
+		float getMaxX() const;
 
+		// NOTE : the bound is a box that entirely contains the shape
+		inline Vector2 getBoundTopLeft()  const { return { getMinX(), getMinY() }; }
+		inline Vector2 getBoundTopRight() const { return { getMaxX(), getMinY() }; }
+		inline Vector2 getBoundBotLeft()  const { return { getMinX(), getMaxY() }; }
+		inline Vector2 getBoundBotRight() const { return { getMaxX(), getMaxY() }; }
+
+		inline float getMinDist()  const { return min( getMinX(), getMinY() ); }
+		inline float getMaxDist()  const { return max( getMaxX(), getMaxY() ); }
+
+		inline float getWidth()    const { return getMaxX() - getMinX(); }
+		inline float getHeight()   const { return getMaxY() - getMinY(); }
+
+		inline float getMaxPerim() const { return 2 * ( getWidth() + getHeight() ); }
+		inline float getMaxArea()  const { return getWidth() * getHeight(); }
+
+		// NOTE : the radius is the distance from the center to the furthest vertex
 		float getMinRadius() const;
 		float getMaxRadius() const;
 		float getAvgRadius() const;
+
+		float getPerim()     const;
+		float getArea()      const;
 
 	// ================================== MUTATORS
 		void setAngle( float angle );           void changeAngle( float delta );
@@ -128,47 +157,48 @@ class Shape2
 		void setScales( const Vector2 &scl );   void changeScales( const Vector2 &factor );
 
 	// ================================== PROPERTIES COMPARISON METHODS
-		bool hasSameX(   const Shape2 &s ) const;
-		bool hasSameY(   const Shape2 &s ) const;
-		bool hasSameCtr( const Shape2 &s ) const;
+		inline bool hasSameX(     const Shape2 &s ) const { return ( _center.x == s._center.x ); };
+		inline bool hasSameY(     const Shape2 &s ) const { return ( _center.y == s._center.y ); };
+		inline bool hasSameCtr(   const Shape2 &s ) const { return ( _center.x == s._center.x && _center.y == s._center.y ); };
+		inline bool hasSameSclX(  const Shape2 &s ) const { return ( _scales.x == s._scales.x ); };
+		inline bool hasSameSclY(  const Shape2 &s ) const { return ( _scales.y == s._scales.y ); };
+		inline bool hasSameScl(   const Shape2 &s ) const { return ( _scales.x == s._scales.x && _scales.y == s._scales.y ); };
 
-		bool hasSameRadX( const Shape2 &s ) const;
-		bool hasSameRadY( const Shape2 &s ) const;
-		bool hasSameRads( const Shape2 &s ) const;
+		inline bool hasSameType(   const Shape2 &s ) const { return ( _type == s._type ); };
+		inline bool hasSameFacetC( const Shape2 &s ) const { return ( _verts.size() == s._verts.size() ); };
+		inline bool hasSameAngle(  const Shape2 &s ) const { return ( _angle == s._angle ); };
 
-		bool hasSameType(  const Shape2 &s ) const;
-		bool hasSameFaces( const Shape2 &s ) const;
-		bool hasSameVerts( const Shape2 &s ) const;
-		bool hasSameAngle( const Shape2 &s ) const;
-		bool hasSameArea(  const Shape2 &s ) const;
-		bool hasSamePerim( const Shape2 &s ) const;
+		inline bool hasSameWidth(  const Shape2 &s ) const { return ( getWidth() == s.getWidth() ); };
+		inline bool hasSameHeight( const Shape2 &s ) const { return ( getHeight() == s.getHeight() ); };
 
-		bool hasSameWidth(  const Shape2 &s ) const;
-		bool hasSameHeight( const Shape2 &s ) const;
+		inline bool hasSameArea( const Shape2 &s ) const { return ( getArea() == s.getArea() ); };
+		inline bool isLarger(    const Shape2 &s ) const { return ( getArea() >  s.getArea() ); };
+		inline bool isSmaller(   const Shape2 &s ) const { return ( getArea() <  s.getArea() ); };
 
-		bool isLarger(  const Shape2 &s ) const; // checks if s1's area is larger than s2
-		bool isSmaller( const Shape2 &s ) const; // checks if s1's area is smaller than s2
-		bool isLonger(  const Shape2 &s ) const; // checks if s1's perimeter is larger than s2
-		bool isShorter( const Shape2 &s ) const; // checks if s1's perimeter is smaller than s2
+		inline bool hasSamePerim( const Shape2 &s ) const { return ( getPerim() == s.getPerim() ); };
+		inline bool isLonger(     const Shape2 &s ) const { return ( getPerim() >  s.getPerim() ); };
+		inline bool isShorter(    const Shape2 &s ) const { return ( getPerim() <  s.getPerim() ); };
 
-		bool isCloserCirc( const Vector2 &p, float dist ) const;
-		bool isCloserCart( const Vector2 &p, float dist ) const;
+		inline bool hasSameMaxRadius( const Shape2 &s ) const { return ( getMaxRadius() == s.getMaxRadius() ); };
+		inline bool hasSameMinRadius( const Shape2 &s ) const { return ( getMinRadius() == s.getMinRadius() ); };
+		inline bool hasSameAvgRadius( const Shape2 &s ) const { return ( getAvgRadius() == s.getAvgRadius() ); };
 
-	// ================================== SHAPE COMPARISON METHODS
+		bool hasSameVerts( const Shape2 &s )  const;
+
+		// ================================== SHAPE COMPARISON METHODS
 		bool overlaps( const Shape2 &s ) const; // checks if s1 overlaps s2
 		bool isOnEdge( const Shape2 &s ) const; // checks if s1 overlaps the edge of s2
 		bool isWithin( const Shape2 &s ) const; // checks if s1 is entirely within s2
 		bool englobes( const Shape2 &s ) const; // checks if s2 is entirely within s1
 
-	// ================================== POINT COMPARISON METHODS
-		float getXDist( const Vector2 &p ) const;
-		float getYDist( const Vector2 &p ) const;
+		// ================================== POINT DISTANCE COMPARISON METHODS
+		inline float getXDist( const Vector2 &p ) const { return abs( p.x - _center.x ); }
+		inline float getYDist( const Vector2 &p ) const { return abs( p.y - _center.y ); }
+		inline float getDist(  const Vector2 &p ) const { return getDistance( p, _center ); }
 
 		float getShortCartDist( const Vector2 &p ) const;
 		float getLongCartDist(  const Vector2 &p ) const;
-
-		float getCartDist( const Vector2 &p ) const;
-		float getCircDist( const Vector2 &p ) const;
+		float getSumCartDist(   const Vector2 &p ) const;
 
 		float getAngleWith(    const Vector2 &p ) const;
 		float getAngleBetween( const Vector2 &p1, const Vector2 &p2 ) const;
@@ -204,7 +234,7 @@ class Shape2
 		friend Shape2 operator*( float lhs, const Shape2 &rhs );
 		friend Shape2 operator*( const Shape2 &lhs, float rhs );
 		friend Shape2 operator/( const Shape2 &lhs, float rhs );
-		friend Shape2 operator%( const Shape2 &lhs, float rhs );
+		//friend Shape2 operator%( const Shape2 &lhs, float rhs );
 
 	// ================================== ASSIGNMENT OPERATORS
 		//friend Shape2 &operator+=( Shape2 &lhs, const Shape2 &rhs );
@@ -213,7 +243,7 @@ class Shape2
 		friend Shape2 &operator*=( float rhs, Shape2 &lhs );
 		friend Shape2 &operator*=( Shape2 &lhs, float rhs );
 		friend Shape2 &operator/=( Shape2 &lhs, float rhs );
-		friend Shape2 &operator%=( Shape2 &lhs, float rhs );
+		//friend Shape2 &operator%=( Shape2 &lhs, float rhs );
 
 };
 
--- srcs/base/maths/shape2.cpp
+++ srcs/base/maths/shape2.cpp
@@ -1,11 +1,12 @@
+#include <cmath>
 #include <raylib.h>
 #include "../../../incs/base.hpp"
 
-V2Vect_t calcPolyVerts( byte_t facets )
+V2Vect_t CalcPolyVerts( byte_t facets )
 {
 	if ( facets < 2 )
 	{
-		log( "calcPolyVerts() : facets must be >= 2", WARN );
+		log( "CalcPolyVerts() : facets must be >= 2", WARN );
 		return V2Vect_t();
 	}
 
@@ -16,10 +17,26 @@ V2Vect_t calcPolyVerts( byte_t facets )
 	return verts;
 }
 
+Vector2 RotateVertex( const Vector2 &v, float angle ) // TODO : test me
+{
+	float x = v.x * cosf( angle * DtoR ) - v.y * sinf( angle * DtoR );
+	float y = v.x * sinf( angle * DtoR ) + v.y * cosf( angle * DtoR );
+
+	return { x, y };
+}
+
+float getDistance(     const Vector2 &p1, const Vector2 &p2 ){ return sqrt( sqr( p1.x - p2.x ) + sqr( p1.y - p2.y )); }
+float getDistanceTo00( const Vector2 &p ){ return sqrt( sqr( p.x ) + sqr( p.y )); }
+
+float getTriangleArea( const Vector2 &p1, const Vector2 &p2, const Vector2 &p3 )
+{
+	return abs( ( p1.x * ( p2.y - p3.y ) + p2.x * ( p3.y - p1.y ) + p3.x * ( p1.y - p2.y )) / 2 );
+}
+
 // ================================== CONSTRUCTORS
 
 Shape2::Shape2( sh2_type_e type, const Vector2 &ctr, const Vector2 &scl, float angle )
-	: _type( type ), _center( ctr ), _scales( scl ), _angle( angle )
+	: _type( type ), _angle( fmod( angle, 360.0f )), _center( ctr ), _scales( { abs( scl.x ), abs( scl.y )})
 {
 	_verts = V2Vect_t();
 	switch ( type )
@@ -60,19 +77,19 @@ Shape2::Shape2( sh2_type_e type, const Vector2 &ctr, const Vector2 &scl, float a
 			break;
 
 		case SH2_TRIA: // makes a upright equilateral triangle
-			_verts = calcPolyVerts( 3 );   break;
+			_verts = CalcPolyVerts( 3 );   break;
 
 		case SH2_PENT: // makes a pointy top pentagon - rotate by 36 degrees for a flat top pentagon
-			_verts = calcPolyVerts( 5 );   break;
+			_verts = CalcPolyVerts( 5 );   break;
 
 		case SH2_HEXA: // makes a pointy top hexagon - rotate by 30 degrees for a flat top hexagon
-			_verts = calcPolyVerts( 6 );   break;
+			_verts = CalcPolyVerts( 6 );   break;
 
 		case SH2_OCTA: // makes a pointy top octagon - rotate by 22.5 degrees for a flat top octagon
-			_verts = calcPolyVerts( 8 );   break;
+			_verts = CalcPolyVerts( 8 );   break;
 
 		case SH2_DODE: // makes a pointy top dodecagon - rotate by 15 degrees for a flat top dodecagon
-			_verts = calcPolyVerts( 12 );  break;
+			_verts = CalcPolyVerts( 12 );  break;
 
 		case SH2_POLY:
 			log( "Shape2::Shape2() : Vertex need to be manually set for polygons", DEBUG );
@@ -85,7 +102,7 @@ Shape2::Shape2( sh2_type_e type, const Vector2 &ctr, const Vector2 &scl, float a
 	}
 }
 
-Shape2::Shape2( const Shape2 &s ) : _type( s._type ), _center( s._center ), _scales( s._scales ), _angle( s._angle )
+Shape2::Shape2( const Shape2 &s ) : _type( s._type ), _angle( fmod( s._angle, 360.0f )), _center( s._center ), _scales( s._scales )
 {
 	_verts = V2Vect_t();
 	for ( auto &v : s._verts ){ _verts.push_back( v ); }
@@ -132,11 +149,11 @@ Shape2 Shape2::Line3( const Vector2 &ctr, const Vector2 &p )
 }
 
 // returns a equilateral triangle with the tip at p1
-Shape2 Shape2::Triangle( const Vector2 &p1, const Vector2 &tip )
+Shape2 Shape2::Triangle( const Vector2 &ctr, const Vector2 &tip ) // TODO : check if this is correct
 {
-	float scale = sqrt( sqr( p1.x - tip.x ) + sqr( p1.y - tip.y ));
-	float angle =  RtoD * atan2( tip.y - p1.y, tip.x - p1.x );
-	return Shape2( SH2_TRIA, tip, { scale, scale }, angle );
+	float scale = getDistance( ctr, tip );
+	float angle = RtoD * atan2( tip.y - ctr.y, tip.x - ctr.x );
+	return Shape2( SH2_TRIA, ctr, { scale, scale }, angle );
 }
 // returns a triangle with the center at ctr and a scale of scl
 Shape2 Shape2::Triangle( const Vector2 &ctr, const Vector2 &scl, float angle )
@@ -152,9 +169,9 @@ Shape2 Shape2::Triangle( const Vector2 &p1, const Vector2 &p2, const Vector2 &p3
 	Vector2 np2 = { p2.x - ctr.x, p2.y - ctr.y };
 	Vector2 np3 = { p3.x - ctr.x, p3.y - ctr.y };
 
-	float d1 = sqrt( sqr( np1.x ) + sqr( np1.y ) ); // TODO : check if this isn't redundant
-	float d2 = sqrt( sqr( np2.x ) + sqr( np2.y ) );
-	float d3 = sqrt( sqr( np3.x ) + sqr( np3.y ) );
+	float d1 = getDistanceTo00( np1 ); // TODO : check if this redundant maths
+	float d2 = getDistanceTo00( np2 );
+	float d3 = getDistanceTo00( np3 );
 	float scl = ( d1 + d2 + d3 ) / 3;
 
 	return Shape2( SH2_TRIA, ctr, { scl, scl }); // TODO : check if this is correct
@@ -164,22 +181,23 @@ Shape2 Shape2::Square( const Vector2 &ctr, float scl, float angle )
 {
 	return Shape2( SH2_SQUR, ctr, { scl, scl }, angle );
 }
-Shape2 Shape2::Square( const Vector2 &ctr, const Vector2 &edge, float angle )
-{ // NOTE : ignores the angle when finding a square that fits
-	// TODO : stop ignoring it
-	float scl = min( edge.x - ctr.x, edge.y - ctr.y );
+Shape2 Shape2::Square( const Vector2 &ctr, const Vector2 &edge, float angle ) // TODO : check if this is correct
+{
+	Vector2 re = { edge.x - ctr.x, edge.y - ctr.y }; //     gets the relative position of the edge from the center
+	re = RotateVertex( re, -angle ); //                  cancels out the rotation of the final shape
+	float scl = max( abs( edge.x ), abs( edge.x ) ); // finds the scale needed for both axes to touch the edge
 	return Shape2( SH2_SQUR, ctr, { scl, scl }, angle );
 }
 Shape2 Shape2::Rectangle( const Vector2 &ctr, const Vector2 &scl, float angle )
 {
 	return Shape2( SH2_RECT, ctr, scl, angle );
 }
-Shape2 Shape2::RectCorners( const Vector2 &p1, const Vector2 &p2, float angle )
-{ // NOTE : ignores the angle when finding a rectangle that fits
-	// TODO : stop ignoring it
-	Vector2 ctr = { ( p1.x + p2.x ) / 2, ( p1.y + p2.y ) / 2 };
-	Vector2 scl = { ( p2.x - p1.x ) / 2, ( p2.y - p1.y ) / 2 };
-	return Shape2( SH2_RECT, ctr, scl, angle );
+Shape2 Shape2::RectCorners( const Vector2 &p1, const Vector2 &p2, float angle ) // TODO : check if this is correct
+{
+	Vector2 ctr = { ( p1.x + p2.x ) / 2, ( p1.y + p2.y ) / 2 }; // finds the center of the rectangle
+	Vector2 rp = { p1.x - ctr.x, p1.y - ctr.y }; //                gets the relative position of a corner from the center
+	rp = RotateVertex( rp, -angle ); //                         cancels out the rotation of the final shape
+	return Shape2( SH2_RECT, ctr, rp, angle ); //              rp == scl here
 }
 
 Shape2 Shape2::Circle( const Vector2 &ctr, float scl )
@@ -188,7 +206,7 @@ Shape2 Shape2::Circle( const Vector2 &ctr, float scl )
 }
 Shape2 Shape2::Circle( const Vector2 &ctr, const Vector2 &edge )
 {
-	float scl = sqrt( sqr( edge.x - ctr.x) + sqr( edge.y - ctr.y ));
+	float scl = getDistance( ctr, edge );
 	return Shape2( SH2_CIRC, ctr, { scl, scl } );
 }
 Shape2 Shape2::Elipse( const Vector2 &ctr, const Vector2 &scl, float angle )
@@ -200,7 +218,7 @@ Shape2 Shape2::Pentagon( const Vector2 &ctr, float scl, float angle ) { return P
 Shape2 Shape2::Pentagon( const Vector2 &ctr, const Vector2 &scl, float angle )
 {
 	Shape2 s( SH2_PENT, ctr, scl, angle );
-	s._verts = calcPolyVerts( 5 );
+	s._verts = CalcPolyVerts( 5 );
 	return s;
 }
 
@@ -208,7 +226,7 @@ Shape2 Shape2::Hexagon( const Vector2 &ctr, float scl, float angle ){ return Hex
 Shape2 Shape2::Hexagon( const Vector2 &ctr, const Vector2 &scl, float angle )
 {
 	Shape2 s( SH2_HEXA, ctr, scl, angle );
-	s._verts = calcPolyVerts( 6 );
+	s._verts = CalcPolyVerts( 6 );
 	return s;
 }
 
@@ -216,7 +234,7 @@ Shape2 Shape2::Octogon( const Vector2 &ctr, float scl, float angle ){ return Oct
 Shape2 Shape2::Octogon( const Vector2 &ctr, const Vector2 &scl, float angle )
 {
 	Shape2 s( SH2_OCTA, ctr, scl, angle );
-	s._verts = calcPolyVerts( 8 );
+	s._verts = CalcPolyVerts( 8 );
 	return s;
 }
 
@@ -224,7 +242,7 @@ Shape2 Shape2::Dodecagon( const Vector2 &ctr, float scl, float angle ){ return D
 Shape2 Shape2::Dodecagon( const Vector2 &ctr, const Vector2 &scl, float angle )
 {
 	Shape2 s( SH2_DODE, ctr, scl, angle );
-	s._verts = calcPolyVerts( 12 );
+	s._verts = CalcPolyVerts( 12 );
 	return s;
 }
 
@@ -238,11 +256,415 @@ Shape2 Shape2::Polygon( const Vector2 &ctr, const Vector2 &scl, int sides, float
 	}
 
 	Shape2 s( SH2_POLY, ctr, scl, angle );
-	s._verts = calcPolyVerts( sides );
+	s._verts = CalcPolyVerts( sides );
 	return s;
 }
 
 // ================================== ACCESSORS
 
+V2Vect_t Shape2::getScaledVerts() const
+{
+	V2Vect_t verts;
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = v;
+		sv.x *= _scales.x;
+		sv.y *= _scales.y;
+		verts.push_back( sv );
+	}
+	return verts;
+}
+V2Vect_t Shape2::getWorldVerts() const
+{
+	V2Vect_t verts;
 
+	for ( auto &v : _verts ){ verts.push_back( RotateVertex( v, _angle )); }
 
+	return verts;
+}
+
+Vector2 Shape2::getRawVertex( int index ) const
+{
+	index %= _verts.size(); // wrap around
+
+	return _verts[ index ];
+}
+Vector2 Shape2::getScaledVertex( int index ) const
+{
+	index %= _verts.size(); // wrap around
+
+	Vector2 v = _verts[ index ];
+	v.x *= _scales.x;
+	v.y *= _scales.y;
+
+	return v;
+}
+Vector2 Shape2::getWorldVertex( int index ) const
+{
+	index %= _verts.size(); // wrap around
+
+	return RotateVertex( _verts[ index ], _angle );
+}
+
+// ================================== SHAPE PROPERTIES
+
+Vector2 Shape2::getTop() const // top = lowest y
+{
+	Vector2 top = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.y < top.y ){ top = sv; }
+	}
+	top.x += _center.x;
+	top.y += _center.y;
+	return top;
+}
+Vector2 Shape2::getBot() const // bottom = highest y
+{
+	Vector2 bot = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.y > bot.y ){ bot = sv; }
+	}
+	bot.x += _center.x;
+	bot.y += _center.y;
+	return bot;
+}
+Vector2 Shape2::getLeft() const // left = lowest x
+{
+	Vector2 left = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x < left.x ){ left = sv; }
+	}
+	left.x += _center.x;
+	left.y += _center.y;
+	return left;
+}
+Vector2 Shape2::getRight() const // right = highest x
+{
+	Vector2 right = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x > right.x ){ right = sv; }
+	}
+	right.x += _center.x;
+	right.y += _center.y;
+	return right;
+}
+
+// TODO : check if these are correct
+Vector2 Shape2::getTopLeft() const // top = lowest y, left = lowest x
+{
+	Vector2 tl = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x + sv.y < tl.x + tl.y ){ tl = sv; }
+	}
+	tl.x += _center.x;
+	tl.y += _center.y;
+	return tl;
+}
+Vector2 Shape2::getTopRight() const // top = lowest y, right = highest x
+{
+	Vector2 tr = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x - sv.y > tr.x - tr.y ){ tr = sv; }
+	}
+	tr.x += _center.x;
+	tr.y += _center.y;
+	return tr;
+}
+Vector2 Shape2::getBotLeft() const // bottom = highest y, left = lowest x
+{
+	Vector2 bl = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.y - sv.x > bl.y - bl.x ){ bl = sv; }
+	}
+	bl.x += _center.x;
+	bl.y += _center.y;
+	return bl;
+}
+Vector2 Shape2::getBotRight() const // bottom = highest y, right = highest x
+{
+	Vector2 br = Vector2();
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x + sv.y > br.x + br.y ){ br = sv; }
+	}
+	br.x += _center.x;
+	br.y += _center.y;
+	return br;
+}
+
+float Shape2::getMinY() const
+{
+	float min = -INFINITY;
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.y < min ){ min = sv.y; }
+	}
+	return min + _center.y;
+}
+float Shape2::getMaxY() const
+{
+	float max = INFINITY;
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.y > max ){ max = sv.y; }
+	}
+	return max + _center.y;
+}
+float Shape2::getMinX() const
+{
+	float min = -INFINITY;
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x < min ){ min = sv.x; }
+	}
+	return min + _center.x;
+}
+float Shape2::getMaxX() const
+{
+	float max = INFINITY;
+	for ( auto &v : _verts )
+	{
+		Vector2 sv = RotateVertex( v, _angle );
+		if ( sv.x > max ){ max = sv.x; }
+	}
+	return max + _center.x;
+}
+
+float Shape2::getMinRadius() const
+{
+	float min = INFINITY;
+	for ( auto &v : _verts )
+	{
+		float r = getDistanceTo00( v );
+		if ( r < min ){ min = r; }
+	}
+	return min;
+}
+float Shape2::getMaxRadius() const
+{
+	float max = -INFINITY;
+	for ( auto &v : _verts )
+	{
+		float r = getDistanceTo00( v );
+		if ( r > max ){ max = r; }
+	}
+	return max;
+}
+float Shape2::getAvgRadius() const
+{
+	float avg = 0;
+	for ( auto &v : _verts )
+	{
+		avg += getDistanceTo00( v );;
+	}
+	return avg / _verts.size();
+}
+
+float Shape2::getPerim() const // TODO : check if this is correct
+{
+	float perim = 0;
+
+	for ( byte_t i = 0; i < ( byte_t )_verts.size(); i++ ) // TODO : check if this is correct
+	{
+		const Vector2 p1 = _verts[ i ];
+		const Vector2 p2 = _verts[( i + 1 ) % _verts.size() ]; // wrap around
+
+		perim += getDistance( p1, p2 );
+	}
+	return perim;
+}
+
+float Shape2::getArea() const // TODO : check if this is correct
+{
+	if ( _verts.size() < 3 )
+	{
+		log( "Shape2::getArea() : Not enough vertices to calculate area", WARN );
+		return 0;
+	}
+
+	float area = 0;
+
+	for ( byte_t i = 0; i < ( byte_t )_verts.size(); i++ ) // Shoelace formula
+	{
+		const Vector2 p1 = _verts[ i ];
+		const Vector2 p2 = _verts[( i + 1 ) % _verts.size() ]; // wrap around
+
+		area += ( p1.x * p2.y ) - ( p2.x * p1.y ); // TODO : check if this is correct
+	}
+	return abs( area / 2 );
+}
+
+// ================================== MUTATORS
+void Shape2::setAngle( float angle ) { _angle = fmod( angle, 360.0f ); }
+void Shape2::changeAngle( float delta )
+{
+	_angle += delta;
+	_angle = fmod( _angle, 360.0f );
+}
+
+void Shape2::setCenter( const Vector2 &ctr ) { _center = ctr; }
+void Shape2::changeCenter( const Vector2 &delta )
+{
+	_center.x += delta.x;
+	_center.y += delta.y;
+}
+
+void Shape2::setScales( const Vector2 &scl )
+{
+	_scales.x = abs( scl.x );
+	_scales.y = abs( scl.y );
+}
+void Shape2::changeScales( const Vector2 &factor )
+{
+	_scales.x *= factor.x;
+	_scales.y *= factor.y;
+}
+
+// ================================== PROPERTIES COMPARISON METHODS
+
+bool Shape2::hasSameVerts( const Shape2 &s ) const
+{ // NOTE : this only checks the vertex positions, not the scales or angles
+	if ( _verts.size() != s._verts.size() ){ return false; }
+
+	for ( byte_t i = 0; i < ( byte_t )_verts.size(); i++ )
+	{
+		if ( _verts[ i ].x != s._verts[ i ].x || _verts[ i ].y != s._verts[ i ].y ){ return false; }
+	}
+	return true;
+}
+
+// ================================== SHAPE COMPARISON METHODS
+
+// ================================== POINT DISTANCE METHODS
+
+float Shape2::getShortCartDist( const Vector2 &p ) const
+{
+	float x = abs( p.x - _center.x );
+	float y = abs( p.y - _center.y );
+	return min( x, y );
+}
+float Shape2::getLongCartDist( const Vector2 &p ) const
+{
+	float x = abs( p.x - _center.x );
+	float y = abs( p.y - _center.y );
+	return max( x, y );
+}
+float Shape2::getSumCartDist( const Vector2 &p ) const
+{
+	float x = abs( p.x - _center.x );
+	float y = abs( p.y - _center.y );
+	return x + y;
+}
+
+float Shape2::getAngleWith( const Vector2 &p ) const
+{
+	float x = p.x - _center.x;
+	float y = p.y - _center.y;
+	return atan2( y, x ) * RtoD;
+}
+float Shape2::getAngleBetween( const Vector2 &p1, const Vector2 &p2 ) const
+{
+	return ( getAngleWith( p1 ) - getAngleWith( p2 ));
+}
+
+// ================================== COMPARISON OPERATORS
+bool operator==( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( !lhs.hasSameType(  rhs )){ return false; }
+	if ( !lhs.hasSameAngle( rhs )){ return false; }
+	if ( !lhs.hasSameScl(   rhs )){ return false; }
+	if ( !lhs.hasSameCtr(   rhs )){ return false; }
+	if ( !lhs.hasSameVerts( rhs )){ return false; }
+	return true;
+}
+bool operator!=( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( !lhs.hasSameType(  rhs )){ return true; }
+	if ( !lhs.hasSameAngle( rhs )){ return true; }
+	if ( !lhs.hasSameScl(   rhs )){ return true; }
+	if ( !lhs.hasSameCtr(   rhs )){ return true; }
+	if ( !lhs.hasSameVerts( rhs )){ return true; }
+	return false;
+}
+
+bool operator> ( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( lhs.getArea() > rhs.getArea() ){ return true; }
+	return false;
+}
+bool operator< ( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( lhs.getArea() < rhs.getArea() ){ return true; }
+	return false;
+}
+bool operator>=( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( lhs.getArea() >= rhs.getArea() ){ return true; }
+	return false;
+}
+bool operator<=( const Shape2 &lhs, const Shape2 &rhs )
+{
+	if ( lhs.getArea() <= rhs.getArea() ){ return true; }
+	return false;
+}
+
+// ================================== ARITHMETIC OPERATORS
+
+Shape2 operator*( float lhs, const Shape2 &rhs )
+{
+	Shape2 s = rhs;
+	s.changeScales( { lhs, lhs });
+	return s;
+}
+Shape2 operator*( const Shape2 &lhs, float rhs )
+{
+	Shape2 s = lhs;
+	s.changeScales( { rhs, rhs });
+	return s;
+}
+Shape2 operator/( const Shape2 &lhs, float rhs )
+{
+	if ( divtest( rhs )) { throw std::invalid_argument( "Invalid shape division" ); }
+
+	Shape2 s = lhs;
+	s.changeScales( { 1 / rhs, 1 / rhs });
+	return s;
+}
+
+// ================================== ASSIGNMENT OPERATORS
+
+Shape2 &operator*=( float rhs, Shape2 &lhs )
+{
+	lhs.changeScales( { rhs, rhs });
+	return lhs;
+}
+Shape2 &operator*=( Shape2 &lhs, float rhs )
+{
+	lhs.changeScales( { rhs, rhs });
+	return lhs;
+}
+Shape2 &operator/=( Shape2 &lhs, float rhs )
+{
+	if ( divtest( rhs )) { throw std::invalid_argument( "Invalid shape division" ); }
+
+	lhs.changeScales( { 1 / rhs, 1 / rhs });
+	return lhs;
+}
